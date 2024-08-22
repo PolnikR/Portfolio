b5e9aefaca3116a1fccf52375cf4dbd3bc8d99b2
Roman <roman.polnik@esas.autocont.sk>
2024-08-22 12:49:52 +0200

    projects

--- IndustryApp/views.py
+++ IndustryApp/views.py
@@ -7,7 +7,7 @@ def industry(request):
     project_details = projects[0].project_details.all()
     print(projects[0].project_icon.url)
     technologies = project_details[0].technologies.all()
-    print(technologies[0].technology_image.url)
+    print(technologies[0].technology_name)
 
     context = {
         'projects': projects
