75a1f19b26d1c8c8d4184806b4af38ea555a1cb6
Roman <roman.polnik@esas.autocont.sk>
2024-08-23 10:24:20 +0200

    home model app updates

--- HomeApp/admin.py
+++ HomeApp/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
-
+from .models import LifeCircumstance, Biography
 # Register your models here.
 
+@admin.register(LifeCircumstance)
+class LifeCircumstanceAdmin(admin.ModelAdmin):
+    list_display = ('name', )
+    search_fields = ('name', )
 
+@admin.register(Biography)
+class BiographyAdmin(admin.ModelAdmin):
+    list_display = ('name', 'life_circumstance', 'job_role')
+    search_fields = ('name', 'life_circumstance__name', 'job_role')
+    list_filter = ('life_circumstance', 'job_role')
 
--- HomeApp/models.py
+++ HomeApp/models.py
@@ -7,5 +7,29 @@ from django.db import models
 
 
 # Create your models here.
+class LifeCircumstance(models.Model):
+    name = models.CharField(max_length=255,  null=True, blank=True)
+    images = models.ImageField(upload_to='media/life_circumstances/', null=True, blank=True )
 
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Life Circumstance"
+        verbose_name_plural = "Life Circumstances"
+
+class Biography(models.Model):
+    life_circumstance = models.ForeignKey(LifeCircumstance, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255,  null=True, blank=True)
+    years = models.PositiveIntegerField(null=True, blank=True)
+    job_role = models.CharField(max_length=255, null=True, blank=True)
+    job_description = models.TextField(null=True, blank=True)
+    what_i_learned = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Biography"
+        verbose_name_plural = "Biographies"
 
